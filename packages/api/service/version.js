bd4267d351429b9befeb5a322e44abb2035e6598
Ferran Neira Jorba <bubexel@gmail.com>
2026-02-26 13:31:38 +0100

    Upgrade to latest serverless

--- packages/api/service/version.js
+++ packages/api/service/version.js
@@ -2,23 +2,17 @@ const packageJson = require('./../package.json');
 
 const LambdaHelper = require('./../common/aws/LambdaHelper');
 
-module.exports = (() => {
-	'use strict';
-
-	return {
-		handler: (event, context, callback) => {
-			LambdaHelper.process('Read service metadata', event, callback, (parser, responder) => {
-				return {
-					service: {
-						name: packageJson.name,
-						description: packageJson.description,
-						environment: process.env.NODE_ENV,
-						version: packageJson.version
-					}
-				};
-			});
-		}
-	};
-})();
+module.exports.handler = (event, context, callback) => {
+	LambdaHelper.process('Read service metadata', event, callback, (parser, responder) => {
+		return {
+			service: {
+				name: packageJson.name,
+				description: packageJson.description,
+				environment: process.env.NODE_ENV,
+				version: packageJson.version
+			}
+		};
+	});
+};
 
 
